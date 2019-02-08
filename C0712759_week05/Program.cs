d3f5777535cf3029f74fe4fb52cc288743fef3e4
visualstudio <visualstudio@122-PC31>
2019-02-08 18:29:03 -0500

    6:10

--- C0712759_week05/Program.cs
+++ C0712759_week05/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace C0712759_week05
+namespace FRI_FEB_8
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Elevator e = new Elevator();
+            e.run();
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Elevator
+    {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void run()
+        {
+            FirstFloor = new Node();
+            FirstFloor.FloorNumber = "First Floor";
+            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+
+            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
         }
     }
 }
