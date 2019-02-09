c84cb95e1a40c82570cfd020de01eeae69b8b00d
visualstudio <visualstudio@122-PC31>
2019-02-08 20:37:55 -0500

    8:30PM

--- C0712759_week05/Program.cs
+++ C0712759_week05/Program.cs
@@ -7,32 +7,39 @@ namespace FRI_FEB_8
         static void Main(string[] args)
         {
             Elevator e = new Elevator();
-            e.run();
+            e.setup();
+            e.TraverseList();
         }
     }
 
     class Node
     {
         public Node() { }
-        public Node floor;
         public Node elevatorUp;
         public string FloorNumber;
     }
 
+    class Department
+    {
+        public Department aDepartment;
+
+    }
+
     class Elevator
     {
+        Node Head;
         Node FirstFloor;
         Node SecondFloor;
         Node ThirdFloor;
         Node FourthFloor;
 
-        public void run()
+        public void setup()
         {
             FirstFloor = new Node();
             SecondFloor = new Node();
             ThirdFloor = new Node();
             FourthFloor = new Node();
-
+            Head = FirstFloor;
             FirstFloor.FloorNumber = "First Floor";
             FirstFloor.elevatorUp = SecondFloor;
             SecondFloor.FloorNumber = "Second Floor";
@@ -41,7 +48,6 @@ namespace FRI_FEB_8
             ThirdFloor.elevatorUp = FourthFloor;
             FourthFloor.FloorNumber = "Fourth Floor";
             FourthFloor.elevatorUp = null;
-
         }
 
         public void TraverseList()
@@ -50,14 +56,14 @@ namespace FRI_FEB_8
             temp = Head;
 
             // where am I going to start?
-            Console.WriteLine("The first floor is " + Head.FloorNumber);
+
             while (temp != null)
             {
-                // NOW GET TO THE SECOND FLOOR!!!!
-                temp = temp.elevatorUp;
                 Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
 
             }
+
         }
     }
 }
